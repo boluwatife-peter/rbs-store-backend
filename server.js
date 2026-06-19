fe5b7b56ca66c8be03f4b5900008a87bffc4f595
boluwatife coder <scholllrota@gmail.com>
2026-06-19 09:21:09 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const Stripe = require("stripe");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
+app.post("/create-checkout-session", async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      mode: "payment",
+
+      line_items: req.body.items.map(item => ({
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: item.name
+          },
+          unit_amount: Math.round(item.price * 100)
+        },
+        quantity: 1
+      })),
+
+      success_url: "https://boluwatife-peter.github.io/rbs-shop/",
+      cancel_url: "https://boluwatife-peter.github.io/rbs-shop/"
+    });
+
+    res.json({
+      url: session.url
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      error: err.message
+    });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
